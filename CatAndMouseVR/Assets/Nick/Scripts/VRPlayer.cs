78535156ba33133ccdf26ad682e7e3bdc86cf4c2
Nick <nwpopie@icloud.com>
2025-03-27 13:21:04 -0400

    Movement that works now :D

--- CatAndMouseVR/Assets/Nick/Scripts/VRPlayer.cs
+++ CatAndMouseVR/Assets/Nick/Scripts/VRPlayer.cs
@@ -1,8 +1,11 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class VRPlayer : MonoBehaviour
 {
+    [Range(1f, 10f)]
+    float speed = 1f;
     [SerializeField]
     VRHand LeftHand;
     [SerializeField]
@@ -23,9 +26,16 @@ public class VRPlayer : MonoBehaviour
         float movement = (LeftHand.movementNum + RightHand.movementNum) / 2;
         if (movement > .01f)
         {
-            Vector3 moveDir = head.GameObject().transform.forward;//(LeftHand.GameObject().transform.up + RightHand.GameObject().transform.up) / 2;
-            moveDir = Vector3.Scale(moveDir, new Vector3(1, 0, 1));
+
+            Vector3 moveDir = -RightHand.GameObject().transform.up + -LeftHand.GameObject().transform.up; //(LeftHand.GameObject().transform.up + RightHand.GameObject().transform.up) / 2;
+            moveDir = moveDir.normalized;
+            moveDir = Vector3.Scale(moveDir, new Vector3(speed, 0, speed));
             cc.Move(moveDir);
         }
+    }
+
+    void OnVRLook(InputValue inv)
+    {
+        transform.Rotate(0, inv.Get<Vector2>().x, 0);
     }
 }

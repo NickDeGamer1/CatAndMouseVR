618fcca2c9f43c2fbe11992450c3f0819e915d73
JoetheSchmo <JoetheShmo12@aol.com>
2025-04-08 13:54:58 -0400

    Cat Collider

---
+++ CatAndMouseVR/Assets/Joe/Scripts/c_CatCollider.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class c_CatCollider : MonoBehaviour
+{
+
+    public c_CatController catAttatched;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<MousePuppet>() != null && catAttatched.currentState == jumpState){
+            Debug.Log("CAT WIN!");
+        }
+    }
+}

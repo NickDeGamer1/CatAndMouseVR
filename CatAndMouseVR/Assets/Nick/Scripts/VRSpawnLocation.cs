71a35fb6b468df7c6bf672cc28654395a8486164
Nick <nwpopie@icloud.com>
2025-04-14 19:01:58 -0400

    SpawnAreaVR

---
+++ CatAndMouseVR/Assets/Nick/Scripts/VRSpawnLocation.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class VRSpawnLocation : MonoBehaviour
+{
+    BoxCollider bc;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        bc = GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("VRPlayer"))
+        {
+            //Emit Game started
+
+            Destroy(gameObject);
+        }
+    }
+}
